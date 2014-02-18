0b1730dffcdb62c008406054b9b439d14ce0fcc3
Mikael Jorhult <mikael@jorhult.se>
2014-02-18 20:49:00 +0100

    Working settings dialog.
    Code is largely based on work by Martin Zaggora (@zaggino).
    
    Resolves #8.

--- modules/SettingsDialog.js
+++ modules/SettingsDialog.js
@@ -11,39 +11,45 @@ define( function( require, exports ) {
 		// Variables.
 		dialog,
 		preferences;
-
+	
+	/**
+	 * Set each value of the preferences in dialog.
+	 */
 	function setValues( values ) {
-		$( '#git-settings-stripWhitespaceFromCommits' ).prop( 'checked', values.stripWhitespaceFromCommits );
+		$( '#autoprefixer-settings-visualCascade' ).prop( 'checked', values.visualCascade );
 	}
-
-	function restorePlatformDefaults() {
-		setValues( DefaultPreferences );
-	}
-
+	
+	/**
+	 * Initialize dialog values.
+	 */
 	function init() {
 		setValues( preferences.getAllValues() );
-		assignActions();
-
-		if (brackets.platform !== 'win' ) {
-			$( '.windows_only' ).hide();
-		}
 	}
-
+	
+	/**
+	 * Exposed method to show dialog.
+	 */
 	exports.show = function( prefs ) {
+		// Compile dialog template.
 		var compiledTemplate = Mustache.render( settingsDialogTemplate, {
 			Strings: Strings
 		} );
-
+		
+		// Save dialog to variable.
 		dialog = Dialogs.showModalDialogUsingTemplate( compiledTemplate );
 		preferences = prefs;
-
+		
+		// Initialize dialog values.
 		init();
-
+		
+		// Open dialog.
 		dialog.done( function( buttonId ) {
+			// Save preferences if OK button was clicked.
 			if ( buttonId === 'ok' ) {
 				var $dialog = dialog.getElement();
-				// features
-				preferences.setValue( 'stripWhitespaceFromCommits', $( '#git-settings-stripWhitespaceFromCommits', $dialog).prop( 'checked' ));
+				
+				// Save each preference.
+				preferences.setValue( 'visualCascade', $( '#autoprefixer-settings-visualCascade', $dialog ).prop( 'checked' ) );
 			}
 		});
 	};
