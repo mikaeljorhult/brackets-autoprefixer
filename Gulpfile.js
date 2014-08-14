ef63bb88481f52b321aa19cf5439179d7ca8b7a5
Mikael Jorhult <mikael@jorhult.se>
2014-08-14 21:03:20 +0200

    Use JSCS to check code style.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require( 'gulp' ),
 	plugins = require( 'gulp-load-plugins' )(),
-	files = [ 'main.js', 'package.json', 'modules/*.js', 'nls/**/*.js' ];
+	files = [ 'main.js', 'modules/*.js', 'nls/**/*.js' ];
 
 gulp.task( 'lint', function() {
 	return gulp.src( files )
 		.pipe( plugins.jshint() )
-		.pipe( plugins.jshint.reporter( 'default' ) );
+		.pipe( plugins.jshint.reporter( 'default' ) )
+		.pipe( plugins.jscs() );
 } );
 
 gulp.task( 'default', [ 'lint' ] );
