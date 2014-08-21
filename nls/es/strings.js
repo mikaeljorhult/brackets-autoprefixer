c6577e112db3757dfe19ce2c46adf5e061a5c018
Iván Barcia <contacto@ivanbarcia.eu>
2014-08-21 11:42:11 +0200

    Create spanish language

---
+++ nls/es/strings.js
@@ -0,0 +1,16 @@
+define( {
+	// Generic.
+	OK:     "OK",
+	CANCEL: "Cancelar",
+	RESET:  "Restablecer",
+	
+	// Menu.
+	MENU_ON_SAVE:   "Prefijo automático al guardar",
+	MENU_SELECTION: "Prefijo automático en la selección",
+	MENU_SETTINGS:  "Ajustes de Autoprefixer...",
+	
+	// Settings dialog.
+	SETTINGS_TITLE:          "Ajustes de Autoprefixer",
+	SETTINGS_VISUAL_CASCADE: "Cascada visual",
+	SETTINGS_BROWSERS:       "Navegadores"
+} );
