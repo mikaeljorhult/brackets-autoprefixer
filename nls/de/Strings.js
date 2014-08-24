c09d096de0c46185ce9cfe20b1354f3423ea67c3
Marcel Gerber <MarcelGerber@users.noreply.github.com>
2014-08-24 11:39:02 +0200

    German Translation

---
+++ nls/de/Strings.js
@@ -0,0 +1,16 @@
+define( {
+	// Generic.
+	OK:     "OK",
+	CANCEL: "Abbrechen",
+	RESET:  "Zurücksetzen",
+	
+	// Menu.
+	MENU_ON_SAVE:   "Automatische Präfixe beim Speichern",
+	MENU_SELECTION: "Automatische Präfixe für die Auswahl",
+	MENU_SETTINGS:  "Autoprefixer-Einstellungen...",
+	
+	// Settings dialog.
+	SETTINGS_TITLE:          "Autoprefixer-Einstellungen",
+	SETTINGS_VISUAL_CASCADE: "Einrückungskaskade",
+	SETTINGS_BROWSERS:       "Browser"
+} );
