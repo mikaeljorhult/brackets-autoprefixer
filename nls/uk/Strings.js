bed1718da11f4e286934644acd7c94cd7d6c6f46
Maks Lyashuk <m_lyashuk@ukr.net>
2014-08-26 13:04:40 +0300

    Create Ukrainian translation

---
+++ nls/uk/Strings.js
@@ -0,0 +1,16 @@
+define( {
+	// Generic.
+	OK:     "Гаразд",
+	CANCEL: "Скасувати",
+	RESET:  "Скинути",
+	
+	// Menu.
+	MENU_ON_SAVE:   "Додавати префікси під час збереження",
+	MENU_SELECTION: "Додавати префікси під час виділення",
+	MENU_SETTINGS:  "Налаштувати Autoprefixer...",
+	
+	// Settings dialog.
+	SETTINGS_TITLE:          "Параметри Autoprefixer",
+	SETTINGS_VISUAL_CASCADE: "Каскадні відступи",
+	SETTINGS_BROWSERS:       "Браузери"
+} );
