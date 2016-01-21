9c03c0a3aeb292fa816534cb5af2195148753b45
Mikael Jorhult <mikael@jorhult.se>
2016-01-21 20:26:38 +0100

    Use Autoprefixer process method for processing.

--- main.js
+++ main.js
@@ -116,17 +116,17 @@ define( function( require ) {
 	/**
 	 * Process text using Autoprefixer.
 	 */
-	function process( originalText ) {
+	function process(originalText) {
 		var processedText = false,
 			browsers = preferences.get( 'browsers' );
 		
 		// Return false if not able to process.
 		try {
-			processedText = autoprefixer( {
+			processedText = autoprefixer.process(originalText, {
 				browsers: browsers.length > 0 ? browsers : Defaults.browsers,
-				cascade: preferences.get( 'visualCascade' )
-			} ).process( originalText ).css;
-		} catch ( e ) {
+				cascade: preferences.get('visualCascade')
+			}).css;
+		} catch (e) {
 			return false;
 		}
 		
